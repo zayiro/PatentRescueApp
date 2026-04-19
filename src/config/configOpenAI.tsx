adfc6480adc1c377232187d20b3eb1e70776c852
Zayr andrés ocampo muñoz <ocampotecnologo@gmail.com>
2026-04-19 12:58:18 -0500

    remve secret

--- src/config/configOpenAI.tsx
+++ src/config/configOpenAI.tsx
@@ -1,7 +1,4 @@
-// config.ts
-//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
-//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
-export const OPENAI_API_KEY = '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'; // Tu API Key
+export const OPENAI_API_KEY = ''; // Tu API Key
 export const OPENAI_BASE_URL = 'https://api.openai.com/v1';
 
 export const MEDICAL_SYSTEM_PROMPT = `
